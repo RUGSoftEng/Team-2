0e0415d84e9cc046ee4ffa1c14e2ab8c55c75fd3
marlies400 <m.e.overdijk@student.rug.nl>
2016-02-28 17:04:53 +0100

    Added files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,16 +1,16 @@
-package com.example.ruben.softwareengeneering20;
+package com.mycompany.myapp;
 
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.support.design.widget.FloatingActionButton;
-        import android.support.design.widget.Snackbar;
-        import android.support.v7.app.AppCompatActivity;
-        import android.support.v7.widget.Toolbar;
-        import android.view.View;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.widget.Button;
-        import android.widget.EditText;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
--- NextActivity.java
+++ NextActivity.java
@@ -1,18 +1,21 @@
-package com.example.ruben.softwareengeneering20;
+package com.mycompany.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-//TODO: Still largly hardcoded
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class NextActivity extends AppCompatActivity {
-    ListView listView;
     ArrayAdapter<String> adapter;   // later on cursoradapter
-    String[] quests = {"Pub quest",  "Cultural quest", "Food quest"}; // need to come from database
+    String[] quests = {"Pub quest", "Cultural quest", "Food quest"}; // need to come from database
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +24,20 @@ public class NextActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        listView = (ListView)findViewById(R.id.list_view);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, quests);
+        listView = (ListView) findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, quests);
         listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getBaseContext(), QuestActivity.class);
+                startActivity(i);
+                //Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                //        Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        //Start a new quest and being able to run it(now hard coded for MVP)
-        ExactQuest q = new ExactQuest("first quest", false);
-        Landmark firstLandmark = new Landmark("Martini Toren", 1);
-        firstLandmark.setInformation(
-                "The martini toren is arguably Groningen's most well know landmark,\n"
-                        + "it has a height of 97 m and is Groningens heighest church \n"
-        );
-        firstLandmark.setPoints(50);
-        firstLandmark.setLocationName("Martinikerkhof 3");
-        q.addLandmark(firstLandmark);
     }
 
 }
