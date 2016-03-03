ffa53c71494fbbbe63e0bd4c69064b73331c8efa
Ruben <gjr.kip@gmail.com>
2016-03-03 11:13:27 +0100

    Update

---
+++ app/src/main/java/com/mycompany/myapp/Achievement.java
@@ -0,0 +1,9 @@
+package com.mycompany.myapp;
+
+/**
+ * Created by Ruben on 23/02/2016.
+ */
+public class Achievement {
+    private String name, description;
+    private int points;
+}
---
+++ app/src/main/java/com/mycompany/myapp/Quest.java
@@ -0,0 +1,43 @@
+package com.mycompany.myapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ruben on 23/02/2016.
+ */
+public abstract class Quest {
+    protected String name;
+    protected ArrayList<Landmark> landmarks, visitedLandmarks;
+    protected boolean isUserGenerated;
+    protected int progress;
+
+    public Quest(String name, boolean isUserGenerated){
+        this.name = name;
+        this.isUserGenerated = isUserGenerated;
+    }
+
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    //for getting progress we don't need the variable (yet)
+    public int getProgress(){
+        return (100/(landmarks.size() / visitedLandmarks.size()));
+    }
+
+
+
+    //Adding landmarks by object now, could be changed to adding(creating) by name?
+
+    public void addLandmark(Landmark landmark){ this.landmarks.add(landmark);  }
+
+    public void isCompleted(Landmark landmark){
+        this.landmarks.add(landmark);
+    }
+
+
+    //TODO should check for string name and other possibly unwanted user input
+    //True is OK, False is a incorrect Quest
+    public boolean validate(){ return true; };
+}
