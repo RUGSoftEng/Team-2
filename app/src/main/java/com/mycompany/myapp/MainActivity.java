f286ba04d1ffe5190a405e3f37c76ee39e373af4
Harrie Lutz <harrielutz@gmail.com>
2016-03-08 20:37:16 +0100

    Added a GPS "my location" button in the AddlandmarkActivity class   (probably bad location sry). Clicking this button shows the latitude and longitude of the current position, and takes care of GPS enabling. Also added MG icons. :P

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -26,8 +26,6 @@ public class MainActivity extends AppCompatActivity {
     private ImageSwitcher imageSwitcher;
     ArrayList<Integer> imgs = new ArrayList<Integer>();
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
