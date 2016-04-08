55b697f35f142dfeb59ffe6408568bad3b06f66c
Irina Gabriela <Irina Gabriela>
2016-04-08 16:33:01 +0200

    OnQuestActivity - older working version; still needs improvement though, crashes at the last landmark.

--- app/src/main/java/com/mycompany/myapp/OnQuestActivity.java
+++ app/src/main/java/com/mycompany/myapp/OnQuestActivity.java
@@ -1,6 +1,14 @@
 package com.mycompany.myapp;
 
 import android.Manifest;
+import android.app.PendingIntent;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,12 +20,19 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -29,6 +44,7 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
 
 /**
  * Created by Ruben on 17/03/2016.
@@ -40,12 +56,18 @@ public class OnQuestActivity extends FragmentActivity implements OnMapReadyCallb
     private ProgressBar mProgress;
 
     private Quest passedQuest;
+    private ArrayList<Geofence> mGeofenceList;
     private Landmark nextLandmark;
+    private GoogleApiClient mGoogleApiClient;
+    private User user;
+    private PendingIntent mGeofencePendingIntent;
     private ListView listView, listView2;
     private GoogleMap mMap;
     private Marker landmarker;
     private Marker mylocmarker;
 
+    private final static int TOTAL_PROGRESS_TIME = 100;
+    private final static int MY_PERMISSIONS_REQUEST_LOCATION = 10;
     private LocationListener locationListener;
     private LocationManager locationManager;
     private Landmark currentTarget;
@@ -59,7 +81,13 @@ public class OnQuestActivity extends FragmentActivity implements OnMapReadyCallb
         passedQuest = (Quest) getIntent().getSerializableExtra("PassedQuest");
         setUpMapIfNeeded();
 
+        // TODO list should not be empty
+        currentTarget = passedQuest.getLandmarks().get(0);
+
         Log.d("TEST", passedQuest.toString());
+
+//        mGeofenceList = new ArrayList<Geofence>();
+//        mGeofencePendingIntent = null;
 
         //Progress of quest
         mProgress = (ProgressBar) findViewById(R.id.progressBar);
@@ -72,6 +100,14 @@ public class OnQuestActivity extends FragmentActivity implements OnMapReadyCallb
         listView2 = (ListView) findViewById(R.id.listView4);
 
         updateListViews(listView, listView2);
+
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .addApi(LocationServices.API)
+//                .build();
+
+
 
         // Initialize the locationManager and locationListener
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
@@ -121,26 +157,101 @@ public class OnQuestActivity extends FragmentActivity implements OnMapReadyCallb
         ArrayAdapter<Landmark> adapter = new ArrayAdapter<Landmark>(this, android.R.layout.simple_list_item_1, getFirstLandmark(user.getActiveQuest()));
         listView.setAdapter(adapter);
 
+        user.getActiveQuest().getLandmarks().remove(0);
         ArrayAdapter<Landmark> adapter2 =
                 new ArrayAdapter<Landmark>(this, android.R.layout.simple_list_item_1, user.getActiveQuest().getLandmarks());
         listView2.setAdapter(adapter2);
-        helper.close();
+        user.getActiveQuest().getLandmarks().add(0, currentTarget);
     }
+
+    //TODO geofence should be implemented and change the passedQuest(in this class) and update it in the database(+ should change progress)
+
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        mGoogleApiClient.connect();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        mGoogleApiClient.disconnect();
+//    }
 
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        DatabaseHelper helper = new DatabaseHelper(getBaseContext());
-        User user = helper.getUser(helper.getReadableDatabase());
-        ArrayAdapter<Landmark> adapter = new ArrayAdapter<Landmark>(this, android.R.layout.simple_list_item_1, getFirstLandmark(user.getActiveQuest())); //TODO: added this (maybe remove this)
-        listView.setAdapter(adapter);
+//        DatabaseHelper helper = new DatabaseHelper(getBaseContext());
+//        User user = helper.getUser(helper.getReadableDatabase());
+//        ArrayAdapter<Landmark> adapter = new ArrayAdapter<Landmark>(this, android.R.layout.simple_list_item_1, getFirstLandmark(user.getActiveQuest()));
+//        listView.setAdapter(adapter);
+//
+//        user.getActiveQuest().getLandmarks().remove(0);
+//        ArrayAdapter<Landmark> adapter2 =
+//                new ArrayAdapter<Landmark>(this, android.R.layout.simple_list_item_1, user.getActiveQuest().getLandmarks());
+//        listView2.setAdapter(adapter2);
+//        user.getActiveQuest().getLandmarks().add(0, currentTarget);
 
-        ArrayAdapter<Landmark> adapter2 = new ArrayAdapter<Landmark>(this, android.R.layout.simple_list_item_1, user.getActiveQuest().getLandmarks());
-        listView2.setAdapter(adapter2);
+//        if (mGeofenceList.isEmpty()) {
+//            addGeofence(this.nextLandmark);
+//        } else {
+//            mGeofenceList.clear(); //TODO might be doing too many work?? also clearing everythings so only 1 geofence could be used at the time
+//            addGeofence(this.nextLandmark);
+//        }
+
+ //       Log.d("TestGeo", "In geofeceList is landmark id: " + mGeofenceList.get(0).getRequestId() + " and nr of geofences: " + mGeofenceList.size() );
     }
 
+
+//    private PendingIntent getGeofencePendingIntent() {
+//        // Reuse the PendingIntent if we already have it.
+//        Log.d("TestGeo", "We had an pending Intent");
+//        if (mGeofencePendingIntent != null) {
+//            return mGeofencePendingIntent;
+//        }
+//        Intent intent = new Intent(this, GeofenceTransistionsIntentService.class);
+//        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+//        // calling addGeofences() and removeGeofences().
+//        return PendingIntent.getService(this, 0, intent, PendingIntent.
+//                FLAG_UPDATE_CURRENT);
+//    }
+//
+//    private GeofencingRequest getGeofencingRequest() {
+//        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+//        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+//        builder.addGeofences(mGeofenceList);
+//        return builder.build();
+//    }
+//
+//    private void addGeofence(Landmark l) {
+//        mGeofenceList.add(new Geofence.Builder()
+//                // Set the request ID of the geofence. This is a string to identify this
+//                // geofence. Each landmark has its own id so the geofence id's will be similair to the landmark id's
+//                .setRequestId(String.valueOf(l.getID()))
+//
+//                .setCircularRegion(
+//                        l.getLocation().latitude,
+//                        l.getLocation().longitude,
+//                        Constants.GEOFENCE_RADIUS_IN_METERS
+//                )
+//                .setExpirationDuration(Constants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+//                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+//                        Geofence.GEOFENCE_TRANSITION_EXIT)
+//                .build());
+//
+//
+//    }
+//
+//    public void deleteGeofence(Landmark l) {
+//        for (Geofence fence : mGeofenceList) {
+//            if (fence.getRequestId() == String.valueOf(l.getID())) {
+//                mGeofenceList.remove(fence);
+//            }
+//        }
+//    }
 
     private Landmark[] getFirstLandmark(Quest q) { //set current Landmark and return an array with that 1 element
         Landmark[] nextLandmarks = new Landmark[1];
@@ -148,6 +259,37 @@ public class OnQuestActivity extends FragmentActivity implements OnMapReadyCallb
         nextLandmarks[0] = this.nextLandmark;
         return nextLandmarks;
     }
+
+//    @Override
+//    public void onConnected(Bundle bundle) {
+//
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        LocationServices.GeofencingApi.addGeofences(
+//                mGoogleApiClient,
+//                getGeofencingRequest(),
+//                getGeofencePendingIntent()
+//        ).setResultCallback(this);
+//        Log.i("CONNECTION", "Connection to GoogleApiClient");
+//    }
+//
+//    @Override
+//    public void onConnectionSuspended(int i) {
+//        Log.i("CONNECTION", "Connection suspended");
+//    }
+//
+//    @Override
+//    public void onConnectionFailed(ConnectionResult connectionResult) {
+//        Log.e("CONNECTION", "Connection failed");
+//    }
+//
+//    @Override
+//    public void onResult(Status status) { //TODO needs to do something still
+//
+//    }
+
+
 
 
     /*
@@ -187,7 +329,7 @@ public class OnQuestActivity extends FragmentActivity implements OnMapReadyCallb
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case Constants.MY_PERMISSIONS_REQUEST_LOCATION: {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -200,7 +342,7 @@ public class OnQuestActivity extends FragmentActivity implements OnMapReadyCallb
         }
     }
 
-//handles landmarks added to the map
+
     private void handleNewLocation(Location location) {
         Log.d(Constants.TAG, location.toString());
 
@@ -208,39 +350,34 @@ public class OnQuestActivity extends FragmentActivity implements OnMapReadyCallb
         double currentLongitude = location.getLongitude();
         LatLng latLng = new LatLng(currentLatitude, currentLongitude);
 
+        if (location.distanceTo(currentTarget.getLocationObject()) < 20) {
 
-            currentTarget = passedQuest.getLandmarks().get(0); //get next Landmark
+            Toast.makeText(getApplicationContext(),
+                    "Reached landmark", Toast.LENGTH_LONG).show();
 
-            if (location.distanceTo(currentTarget.getLocationObject()) < Constants.MAXIMAL_ACTIVATION_DISTANCE) {
+            Intent i = new Intent(getBaseContext(), LandMarkPopUpActivity.class);
+            i.putExtra("passedLandmark", currentTarget);
+            startActivity(i);
 
-                Toast.makeText(getApplicationContext(),
-                        Constants.COMPLETED_LANDMARK_TEXT, Constants.FINISHED_LANDMARK_DURATION).show();
-
-                Intent i = new Intent(getBaseContext(), LandMarkPopUpActivity.class);
-                i.putExtra("passedLandmark", currentTarget);
-                startActivity(i);
-
-                DatabaseHelper helper = new DatabaseHelper(getBaseContext());
-                User user = helper.getUser(helper.getReadableDatabase());
-                user.getActiveQuest().isCompleted(user.getActiveQuest().getLandmarks().get(0)); //TODO: get(0) should be changed to work for non ordered quests
-                if (user.getActiveQuest().getLandmarks().isEmpty()) {
-                    // end of quest
-                    user.finishQuest(user.getActiveQuest());
-
-                    Intent in = new Intent(getBaseContext(), QuestFinishedActivity.class);
-                    in.putExtra("passedQuest", passedQuest);
-                    startActivity(in);
-
-                } else {
-
-                    currentTarget = user.getActiveQuest().getLandmarks().get(0);
-                    landmarker.setPosition(currentTarget.getLocation());
-                    landmarker.setTitle(currentTarget.getName());
-
-                    updateListViews(listView, listView2);
-                }
+            DatabaseHelper helper = new DatabaseHelper(getBaseContext());
+            User user = helper.getUser(helper.getReadableDatabase());
+            user.getActiveQuest().getLandmarks().remove(0);
+            if (user.getActiveQuest().getLandmarks().isEmpty()) {
+                // end of quest
+                user.finishQuest(user.getActiveQuest());
+                Intent in = new Intent(getBaseContext(), QuestFinishedActivity.class);
+                in.putExtra("finishedQuest", passedQuest);
+                startActivity(in);
+            } else {
+                user.getActiveQuest().getVisitedLandmarks().add(currentTarget);
                 helper.updateInDatabase(helper, user);
-                helper.close();
+
+                currentTarget = user.getActiveQuest().getLandmarks().get(0);
+                landmarker.setPosition(currentTarget.getLocation());
+                landmarker.setTitle(currentTarget.getName());
+
+                updateListViews(listView, listView2);
+            }
         }
 
         if (mylocmarker ==  null) {
